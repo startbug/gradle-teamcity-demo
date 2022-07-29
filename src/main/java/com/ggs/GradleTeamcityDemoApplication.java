d474d20d2d0e3f52548064594ecefd2f539fbe98
startbug <1285226024@qq.com>
2022-07-29 22:31:48 +0800

    初始化项目

---
+++ src/main/java/com/ggs/GradleTeamcityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ggs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTeamcityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleTeamcityDemoApplication.class, args);
+    }
+
+}
